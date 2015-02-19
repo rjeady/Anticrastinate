80a3b4e2cfd9da2986508945c0a5065ee364b409
rjeady <robertj.eady@gmail.com>
2015-02-19 00:17:41 +0000

    Simple program blocker application created.

---
+++ ProgramBlocker/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Windows.Forms;
+
+namespace ProgramBlocker
+{
+	internal class Program
+	{
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+
+			if (args.Length == 1 && args[0].EndsWith(".exe"))
+			{
+				MessageBox.Show(string.Format("Anticrastinate is currently blocking {0} \nShouldn't you be working?", args[0].Split('\\').Last()),
+					"Program Blocked", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+			}
+		}
+	}
+}
+
+
