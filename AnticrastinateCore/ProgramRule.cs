66dd8c3b03655f3251ccf8fb243fd8b932e05177
rjeady <robertj.eady@gmail.com>
2015-02-25 22:33:09 +0000

    Implemented value equality members for ProgramRule class; Created ProgramRuleEnforcer class.

--- AnticrastinateCore/ProgramRule.cs
+++ AnticrastinateCore/ProgramRule.cs
@@ -2,7 +2,7 @@
 
 namespace AnticrastinateCore
 {
-    class ProgramRule
+    class ProgramRule : IEquatable<ProgramRule>
     {
         /// <summary>
         /// Initializes a new instance of the ProgramRule class.
@@ -36,5 +36,32 @@ namespace AnticrastinateCore
         /// The complete file path of the program. May be null if all programs of the same name are to be blocked.
         /// </summary>
         public String Path { get; private set; }
+
+        #region Value Equality Members
+        
+        public bool Equals(ProgramRule other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase) && string.Equals(Path, other.Path, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ProgramRule) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (StringComparer.OrdinalIgnoreCase.GetHashCode(Name)*397) ^ (Path != null ? StringComparer.OrdinalIgnoreCase.GetHashCode(Path) : 0);
+            }
+        }
+
+        #endregion
     }
 }
---
+++ AnticrastinateCore/ProgramRuleEnforcer.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using Microsoft.Win32;
+
+namespace AnticrastinateCore
+{
+    class ProgramRuleEnforcer
+    {
+        private const string RootKeyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options";
+        private const string BlockingKeyName = "Debugger";
+        // TODO: obtain correct path to program blocker executable.
+        private const string BlockingKeyValue = @"C:\ProgramBlocker.exe";
+
+        public void Enforce(RuleSet newRuleSet, RuleSet oldRuleSet)
+        {
+            // compute a rule delta
+            // unblock programs that are only in the old RuleSet
+            var unblockPrograms = oldRuleSet.BlockedPrograms.Except(newRuleSet.BlockedPrograms);
+            // block programs that are only in the new RuleSet
+            var blockPrograms = newRuleSet.BlockedPrograms.Except(oldRuleSet.BlockedPrograms);
+
+            foreach (var program in unblockPrograms)
+                Unblock(program);
+
+            foreach (var program in blockPrograms)
+                Block(program);
+        }
+
+        private void Block(ProgramRule program)
+        {
+            var baseKey = Registry.LocalMachine.OpenSubKey(RootKeyPath, true);
+
+            // will open sub-key if it already exists
+            var appKey = baseKey.CreateSubKey(program.Name);
+            appKey.SetValue(BlockingKeyName, BlockingKeyValue);
+        }
+
+        private void Unblock(ProgramRule program)
+        {
+            var baseKey = Registry.LocalMachine.OpenSubKey(RootKeyPath, true);
+
+            // try to open subkey, null if it doesn't exist (so we don't need to do anything).
+            var appKey = baseKey.OpenSubKey(program.Name, true);
+            if (appKey != null)
+            {
+                // no need to throw if the value doesn't exist, this is good news.
+                appKey.DeleteValue(BlockingKeyName, throwOnMissingValue: false);
+            }
+            
+        }
+
+    }
+}
