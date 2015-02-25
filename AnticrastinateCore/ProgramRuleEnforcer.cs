b1f601ff8ddd5b12b95d89fdc0f30d3e087a9ef5
rjeady <robertj.eady@gmail.com>
2015-02-25 22:44:37 +0000

    ProgramRuleEnforced: added some documentation, and noted failure cases that we will need to handle.

--- AnticrastinateCore/ProgramRuleEnforcer.cs
+++ AnticrastinateCore/ProgramRuleEnforcer.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.Win32;
 
 namespace AnticrastinateCore
 {
+    /// <summary>
+    /// Enforces program RuleSets, using the registry to block programs, by launching our specified executable as a 'debugger' instead.
+    /// </summary>
     class ProgramRuleEnforcer
     {
         private const string RootKeyPath = @"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options";
@@ -28,8 +30,12 @@ namespace AnticrastinateCore
             var baseKey = Registry.LocalMachine.OpenSubKey(RootKeyPath, true);
 
             // will open sub-key if it already exists
-            var appKey = baseKey.CreateSubKey(program.Name);
-            appKey.SetValue(BlockingKeyName, BlockingKeyValue);
+            var programKey = baseKey.CreateSubKey(program.Name);
+            programKey.SetValue(BlockingKeyName, BlockingKeyValue);
+
+            // TODO: handle failures cases -
+            // Image File Exceution Options key couldn't be opened,
+            // or program key coouldn't be created.
         }
 
         private void Unblock(ProgramRule program)
@@ -37,13 +43,15 @@ namespace AnticrastinateCore
             var baseKey = Registry.LocalMachine.OpenSubKey(RootKeyPath, true);
 
             // try to open subkey, null if it doesn't exist (so we don't need to do anything).
-            var appKey = baseKey.OpenSubKey(program.Name, true);
-            if (appKey != null)
+            var programKey = baseKey.OpenSubKey(program.Name, true);
+            if (programKey != null)
             {
                 // no need to throw if the value doesn't exist, this is good news.
-                appKey.DeleteValue(BlockingKeyName, throwOnMissingValue: false);
+                programKey.DeleteValue(BlockingKeyName, throwOnMissingValue: false);
             }
-            
+
+            // TODO: handle failures case -
+            // Image File Exceution Options key couldn't be opened.
         }
 
     }
