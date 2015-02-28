928a3cf65a6be54a4ac0189e8a131b7de972d7e6
rjeady <robertj.eady@gmail.com>
2015-02-28 16:17:00 +0000

    Made IPC testing projects (client and server console applications implemented).

---
+++ IPCTestingClient/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.ServiceModel;
+
+namespace IPCTestingClient
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    internal static class Program
+    {
+        private static readonly Stopwatch Sw = new Stopwatch();
+
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Client? Bitch I might be.");
+
+            // UseWcf();
+            UseStreams();
+        }
+
+        private static void UseWcf()
+        {
+            ChannelFactory<IStringReverser> pipeFactory = new ChannelFactory<IStringReverser>(
+                new NetNamedPipeBinding(), new EndpointAddress("net.pipe://localhost/PipeReverse"));
+
+            IStringReverser pipeProxy = null;
+            Console.WriteLine("Create channel time: {0}ms", Time(Sw, () => { pipeProxy = pipeFactory.CreateChannel(); }));
+
+            while (true)
+            {
+                string str = Console.ReadLine();
+                Console.WriteLine("response took {0}ms", Time(Sw, () => Console.WriteLine("reversed: {0}", pipeProxy.ReverseString(str))));
+            }
+        }
+
+        private static void UseStreams()
+        {
+            using (var clientStream = new NamedPipeClientStream("reversi2"))
+            {
+                clientStream.Connect();
+                using (StreamReader sr = new StreamReader(clientStream))
+                using (StreamWriter sw = new StreamWriter(clientStream) {AutoFlush = true})
+                {
+                    string str;
+                    while ((str = Console.ReadLine()) != "")
+                    {
+                        Console.WriteLine("response took {0}ms", Time(Sw, () =>
+                        {
+                            sw.WriteLine(str);
+                            Console.WriteLine("reversed: {0}", sr.ReadLine());
+                        }));
+                    }
+                }    
+            }
+        }
+
+        private static double Time(Stopwatch sw, Action a)
+        {
+            sw.Restart();
+            a();
+            sw.Stop();
+            return 1000 * (double)sw.ElapsedTicks / Stopwatch.Frequency;
+        }
+    }
+}
---
+++ IPCTestingServer/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.IO.Pipes;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace IPCTestingServer
+{
+    [ServiceContract]
+    public interface IStringReverser
+    {
+        [OperationContract]
+        string ReverseString(string value);
+    }
+
+    public class StringReverser : IStringReverser
+    {
+        public string ReverseString(string value)
+        {
+            var retVal = value.ToCharArray();
+            var idx = 0;
+            for (var i = value.Length - 1; i >= 0; i--)
+                retVal[idx++] = value[i];
+
+            return new string(retVal);
+        }
+    }
+
+    internal static class Program
+    {
+        private static StringReverser stringReverser = new StringReverser();
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        private static void Main()
+        {
+            Console.WriteLine("Server? Bitch I might be.");
+
+            // UseWCF();
+            UseStreams();
+        }
+
+        private static void UseWCF()
+        {
+            using (var host = new ServiceHost(typeof (StringReverser), new Uri("net.pipe://localhost")))
+            {
+                host.AddServiceEndpoint(typeof (IStringReverser), new NetNamedPipeBinding(), "PipeReverse");
+
+                host.Open();
+
+                Console.WriteLine("Service is available. Press <ENTER> to exit.");
+                Console.ReadLine();
+
+                host.Close();
+            }
+        }
+
+        private static void UseStreams()
+        {
+            while (true)
+            {
+                NamedPipeServerStream stream =
+                    new NamedPipeServerStream("reversi2", PipeDirection.InOut, -1, PipeTransmissionMode.Byte);
+
+                stream.WaitForConnection();
+                Task.Factory.StartNew(() => Process(stream));
+            }
+        }
+
+        private static void Process(NamedPipeServerStream stream)
+        {
+            using (stream)
+            using (var sr = new StreamReader(stream))
+            using (var sw = new StreamWriter(stream) {AutoFlush = true})
+            {
+                string request;
+                while ((request = sr.ReadLine()) != null)
+                {
+                    var response = stringReverser.ReverseString(request);
+                    sw.WriteLine(response);
+                }
+                Console.WriteLine("Client disconnected.");
+            }
+        }
+    }
+}
