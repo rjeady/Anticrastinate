15cc436b59482d86dc93bd507bb54c95153dc7cf
rjeady <robertj.eady@gmail.com>
2015-03-21 14:37:03 +0000

    Created ProgramFinder class for finding installed programs.

---
+++ AnticrastinateCore/ProgramFinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnticrastinateCore
+{
+    class ProgramFinder
+    {
+        public static IEnumerable<string> GetPrograms()
+        {
+            string commonFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonStartMenu);
+            IEnumerable<string> files = Directory.EnumerateFiles(commonFolder);
+
+            return files;
+
+        } 
+    }
+}
